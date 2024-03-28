2c76209b901b383e8e240bb2a46b1be08cc2e1be
akarshanyadav8116 <111714329+akarshanyadav8116@users.noreply.github.com>
2024-03-28 22:22:19 +0530

    Add files via upload

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import TextInput from "./TextInput";
+import Loading from "./Loading";
+import CustomButton from "./CustomButton";
+import TopBar from "./TopBar";
+import ProfileCard from "./ProfileCard";
+import FriendsCard from "./FriendsCard";
+export {Loading,TextInput,CustomButton,TopBar,ProfileCard,FriendsCard};
